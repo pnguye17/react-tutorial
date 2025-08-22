6e465b914548ad053e48a66a40910c376e89d31c
pnguye17 <peter.nguyen17@gmail.com>
2025-08-22 13:15:00 -0700

    completed context API toogle

--- src/App.js
+++ src/App.js
@@ -1,19 +1,56 @@
 import './App.css';
+import React, { useState, useContext, createContext } from 'react';
 
 const languages = ["JS", "Ruby"];
 
+//create context
+
+const LanguageContext = createContext(null);
+
+// const LanguageProvider = ({children}) => {
+  
+//   const toggleLanguage = () => {
+//     console.log("toggle fuction hit")
+    
+//     return (
+//       <LanguageContext.Provider value={{language, toggleLanguage}}>
+//       {children}
+//       </LanguageContext.Provider>
+//     )
+//   }
+// }
+
 function App() {
+  
+  const [language, setLanguage] = useState("null");
+  
+  const toggle = () => {
+    console.log("toggle button pressed")
+    setLanguage(
+      (prevLang) => (prevLang === 'Java'? 'Ruby' : 'Java' )
+    )
+
+  }
   return (
-   <Home></Home>
+    <LanguageContext.Provider value={{language, toggle}}>
+     <Home/>    
+
+    </LanguageContext.Provider>
+
+   
   );
 }
 
 const Home = () => {
+  const {language, toggle} = useContext(LanguageContext);
 
   return (
     <>
-      <p> Language is {languages[0]}</p>
-      <button>click me to change the Language</button>
+
+            <p> Language is {language}</p>
+            <button onClick={toggle}>click me to change the Language</button>
+
+
     </>
   )
 }
